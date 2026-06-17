ead9b72f1704ac9e8528cf6a7f22adb6720f8c2d
Naahee Mohamed <naahee.mohamedd@gmail.com>
2026-06-17 10:52:47 +0000

    Fix 2 — Update middleware.ts
    In middleware.ts, remove this part:
    
    // Re

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: [
+      '*.cloudworkstations.dev',
+    ]
+  },
   images: {
     remotePatterns: [
       {
